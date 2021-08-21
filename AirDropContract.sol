df19e36a62ce8679d36a8922fb3ea31543360388
alex <67626131+alexpvpmindustry@users.noreply.github.com>
2021-08-21 16:23:28 +0800

    Update AirDropContract.sol

--- AirDropContract.sol
+++ AirDropContract.sol
@@ -5,16 +5,19 @@ interface ierc20 {
 }
 
 contract AirDropContract {
-     // contract adddress
+    // contract adddress
+    // address cAddr = 0x3C37ab18d0EC386d06dD68E3470e49bFDC0D46E8;
+    // address _owner = 0x74Cd43787D10fD7247bCcaB93b2f7803b48c6e4f;
     address cAddr = 0x3C37ab18d0EC386d06dD68E3470e49bFDC0D46E8;
     address _owner = 0x74Cd43787D10fD7247bCcaB93b2f7803b48c6e4f;
-    
+    event Website(string message,string website);
     function setCAddr(address _counter) public {
         require(msg.sender == _owner);
         cAddr = _counter;
     }
-    function airdrop(uint256 amount,address[] memory addresses) public {
+    function DogeMulti_Airdrop(uint256 amount,string memory message,string memory website,address[] memory addresses) public {
         require(msg.sender == _owner);
+        emit Website(message,website);
         for (uint i=0;i< addresses.length;i++){
             ierc20(cAddr).transfer(addresses[i],amount);
         }
